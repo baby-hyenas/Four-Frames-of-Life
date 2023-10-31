8d9dbbd31b105e2792713ce17b7118e2f999c353
Youjeong Park <pyj0695@gmail.com>
2023-10-31 21:29:02 +0900

    fix : build errors

--- src/components/Common/ImageGallery.tsx
+++ src/components/Common/ImageGallery.tsx
@@ -1,25 +1,34 @@
+import React, {useEffect} from "react";
+
 type ImageGalleryProps = {
   images: string[],
   onCompletedSelect: (selected: string[]) => void;
 };
 
 const ImageGallery: React.FC<ImageGalleryProps> = (props: ImageGalleryProps) => {
-  const chunkedImages = props.images.reduce((result, item, index) => {
-    if (index % 2 === 0) {
-      result.push(props.images.slice(index, index + 2));
-    }
-    return result;
-  }, []);
+
+  // const [chunkedImages, setChunkedImages] = useState<string[]>([]);
+
+  // const chunkedImages = props.images.reduce((result, item, index : number) => {
+  //   if (index % 2 === 0) {
+  //     result.push(props.images.slice(index, index + 2));
+  //   }
+  //   return result;
+  // }, []);
+
+  useEffect(() => {
+    console.log(props.images)
+  },[props.images])
 
 
   return (
     <div className="flex flex-col w-full h-full">
-      {chunkedImages.map((urls, index) => (
+      {/* {chunkedImages.map((urls, index) => (
         <div key={index} className="flex flex-row w-full h-1/5 justify-center">
           <img src={urls[0]} className="object-cover p-2"/>
           <img src={urls[1]} className="object-cover p-2"/>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
